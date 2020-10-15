897ba2d63186baa620934ef25a672259083c2d82
Gabriel nzali <gabriel.nzali@gmail.com>
2020-10-15 14:48:59 +0200

    Pokédex Final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const list = document.getElementById("list");
 const description = document.getElementById("description");
 
-const api = "https://pokeapi.co/api/v2/pokemon?limit=150";
+const api = "https://pokeapi.co/api/v2/pokemon?limit=49";
 
 /**
  * Try to parse a response as JSON data
@@ -14,28 +14,56 @@ function transformToJson (response) {
     throw Error("Content not loaded");
 }
 
+
+
 /**
  * Clear the list of all its items
  */
 function emptyList () {
     // ...
+    
+
 }
 
-/**
+/** 
  * Create an item, fetch its data and setup event listener
  */
 function createItem (pokemon) {
-    // Create a li tag
-    const item = document.createElement("li");
-    // ...
-    fetch(pokemon.url).then(transformToJson).then((data) => {
+    // Create a li
+    const item = document.createElement('li');
+    const image = document.createElement('img');
+    const id = document.createElement('span');
+    const span = document.createElement('span');
+   
+
+    fetch(pokemon.url).then(transformToJson).then((infoPokemon) => {
         // ...
+
+      list.appendChild(item)
+      //infoPokemon.sprites.front_default
+      
+      image.src = infoPokemon.sprites.other["official-artwork"].front_default
+      item.appendChild(image)
+      
+      id.innerText = infoPokemon.id;
+      item.appendChild(id);
+
+      span.innerText = infoPokemon.name
+      item.appendChild(span);
+ 
+      item.addEventListener("click" ,()=>{
+          showDescription(infoPokemon);
+      });
+      
+      console.log(infoPokemon);
     });
 }
 
 /**
  * fill the item list with values
  */
+
+
 function fillList (json) {
     emptyList();
     json.results.forEach(createItem);
@@ -44,12 +72,26 @@ function fillList (json) {
 /**
  * Fill and display the description
  */
-function showDescription (data) {
+function showDescription (infoPokemon) {
     description.classList.add("show");
+
 
     const fields = description.querySelectorAll("dd");
     fields.forEach((dd) => {
-        // ...
+    var classe = dd.classList[0];
+    if(classe != "types"){
+    dd.innerText = infoPokemon[classe];
+    }else{
+        dd.innerText = "";
+        infoPokemon.types.forEach(type => {
+            if(dd.innerText != ""){
+                dd.innerText += ", ";  
+            }
+            dd.innerText += type.type.name;
+            
+        });
+    }
+        
     });
 }
 
@@ -58,6 +100,8 @@ function showDescription (data) {
  */
 function hideDescription () {
     description.classList.remove("show");
+
+    
 }
 
 // Fetch the API end-point and fill the list
